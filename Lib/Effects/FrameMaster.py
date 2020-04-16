cdf051965668556e8ef7c92b3c8c0128ceae7396
unknown <siebrecht12@gmail.com>
2020-04-17 00:58:49 +0200

    Automated Message

--- Lib/Effects/FrameMaster.py
+++ Lib/Effects/FrameMaster.py
@@ -14,38 +14,32 @@ class FrameMaster(SubEngine):
         self.framebufferSize = framebuffer
         self.display = None
         self.load = None
-
-        self.waitingEffekt = Fading()
-        self.subPipe = FakePipe()
         self.waitingObj = Object()
-        self.addObj(self.waitingObj, layer=0)
 
     def setup(self):
+        self.waitingObj.content = [[100,0,0]]*self.pixellength
+        self.addObj(self.waitingObj, layer=0)
+
         self.load = Loading(self.pixellength, 0, self.framebufferSize)
         self.addObj(self.load, layer=1)
+
+
 
         self.display = Display(self.pixellength, 0, self.port, ip=self.ip, framebuffer=self.framebufferSize)
         self.display.setStreamTimeout(5)
         self.display.startServer()
         self.addObj(self.display, layer=2)
 
-        self.waitingEffekt.configur(self.subPipe, self.pixellength)
-        self.waitingEffekt.setup()
-
     def terminate(self):
         self.display.stopServer()
 
     def update(self):
         if self.display.isConnected:
-            if self.waitingObj.isVisible:
-                self.waitingObj.isVisible = False
+            self.waitingObj.isVisible = False
             self.display.update()
             if self.display.isBuffering:
                 self.load.set(len(self.display.framebuffer))
             else:
                 self.load.clear()
         else:
-            if not self.waitingObj.isVisible:
-                self.waitingObj.isVisible = True
-            self.waitingEffekt.sendFrame()
-            self.waitingObj.content = self.subPipe.getFrame()
+            self.waitingObj.isVisible = True
