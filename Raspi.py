e4293d038eb1cd841d04e63a717c090497f9dba6
Jan <54033187+Jks23456@users.noreply.github.com>
2021-03-02 22:00:26 +0100

    Raspi update

--- Raspi.py
+++ Raspi.py
@@ -1,21 +1,17 @@
-from Lib.Controller.StripArrangement import StripArrangement
 from Lib.Effects.FrameMaster import FrameMaster
 from Lib.Effects.Fading import Fading
 from Lib.Engine import Engine
-
+from Lib.Controller.StripArrangement import StripArrangement
 from Lib.Objects.Object import Object
 
 
-
-
 if __name__ == '__main__':
-    strip = StripArrangement()
-    strip.addStrip(245, 13, 11, 1, False)
-    strip.addStrip(205, 18, 10, 0, True)
-
     eng = Engine()
+    strip = StripArrangement()
+    strip.addStrip(207, 18, 10, 0, False)
+
     eng.setControler(strip)
-    eng.startMQTT("strip")
-    eng.addSubEngine(FrameMaster(6501, ip="192.168.2.121"), True)
-    #eng.addSubEngine(Fading(), False)
+    #eng.startMQTT("strip")
+    eng.addSubEngine(FrameMaster(6501, ip="192.168.2.121", framebuffer=30), True)
+    #eng.addSubEngine(Fading(), True)
     eng.run()
